e592acc4e352a66d26bf1a7ce9b9c3fe506d9477
prejudicejack <SavinaVG@yandex.ru>
2023-10-30 22:49:14 +0700

    dedicated class for session management

--- fixture/application.py
+++ fixture/application.py
@@ -1,27 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from fixture.session import SessionHelper
 
 
 class Application:
     def __init__(self):
         self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+        self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def login(self, user_name, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element(By.NAME, "user").click()
-        wd.find_element(By.NAME, "user").clear()
-        wd.find_element(By.NAME, "user").send_keys(user_name)
-        wd.find_element(By.NAME, "pass").click()
-        wd.find_element(By.NAME, "pass").clear()
-        wd.find_element(By.NAME, "pass").send_keys(password)
-        wd.find_element(By.XPATH, "//input[@value='Login']").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -151,10 +142,6 @@ class Application:
     def return_to_groups_page(self):
         wd = self.wd
         wd.find_element(By.LINK_TEXT, "group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element(By.LINK_TEXT, "Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,13 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-    app.login(user_name="admin", password="secret")
+    app.session.login(user_name="admin", password="secret")
     app.create_group(Group(name="test_group", header="some header", footer="some footer"))
-    # app.create_group(group_name="test_group", group_header="some header", group_footer="some footer")
-    app.logout()
+    app.session.logout()
 
 
 def test_add_empty_group(app):
-    app.login(user_name="admin", password="secret")
+    app.session.login(user_name="admin", password="secret")
     app.create_group(Group(name="", header="", footer=""))
-    app.logout()
+    app.session.logout()
